2ee868bd6f382056e5eb9f6b1ef9284261ba6f5c
alex-projectcollection <66368135+alex-projectcollection@users.noreply.github.com>
2020-06-03 15:34:31 +0300

    Add files via upload

---
+++ main_comand.java
@@ -0,0 +1,14 @@
+package game_of_life;
+
+import javax.swing.JFrame;
+
+public class main_comand {
+
+	public static void main(String[] args) {
+
+		world newWorld= new world();
+		newWorld.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
+
+}
